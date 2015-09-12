c9e2d539e5a881799289ac949ce2fa12b87bcc8c
brian <bapaden@mit.edu>
2015-09-12 13:30:12 -0400

    added armijo step and fixed bug in scalarMultiply

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <cstdio>
 #include <vector>
 #include <array>
 #include <cmath>
@@ -13,7 +13,7 @@
 /* State of the algorithm includes the dim of state space n, 
   function that is optimized f, current arg value x and current gradient df */ 
 
-std::vector<double> add(std::vector<double>& x, std::vector<double>& y)
+std::vector<double> add(const std::vector<double>& x,const std::vector<double>& y)
 {
     std::vector<double> z(x.size());
     if(x.size()!=y.size())
@@ -31,32 +31,33 @@ std::vector<double> add(std::vector<double>& x, std::vector<double>& y)
     return z;
 }
 
-std::vector<double> dot(std::vector<double>& x, std::vector<double>& y)
+double dot(const std::vector<double>& x,const std::vector<double>& y)
 {
-    std::vector<double> z(x.size());
+    double z;
     if(x.size()!=y.size())
     {
-        printf("Adding vectors of mismatched size\n");
+        printf("Inner product of vectors of mismatched size\n");
         assert(x.size()==y.size());
     }   
     else
     { 
         for(int i=0;i<x.size();i++)
         {
-            z.at(i)=x.at(i)*y.at(i);
+            z += x.at(i)*y.at(i);
         }
     }
     return z;
 }
 
-std::vector<double> scalarMult(double c, std::vector<double>& x)
+std::vector<double> scalarMult(const double c,const std::vector<double>& x)
 { 
+    std::vector<double> y=x;
     for(int i=0;i<x.size();i++)
     {
-        x.at(i)*=c;
+        y.at(i)*=c;
     }
     
-    return x;
+    return y;
 }
 
 class algState
@@ -79,10 +80,13 @@ public:
     }
 };
 
-class lineSearch
+class gradDescent
 {
 public:
     algState* state_ptr;
+    double armijo_coeff;
+    double step_factor;
+    double step_size;//TODO add constructor which sets defaults
 
     void update_state(algState& state)
     {
@@ -96,11 +100,47 @@ public:
         std::vector<double> temp1=scalarMult(s,df);
         std::vector<double> temp2=add(x,temp1);
         
-        
         return state_ptr->f(temp2);
-        //return state_ptr->f(add(x,scalarMult(s,df)));
     }
-};
+    
+    void armijo_step()
+    {
+      std::vector<double>* df_x = &(state_ptr->df);
+      std::vector<double>* xold = &(state_ptr->x);
+      std::vector<double> xnew = add(*xold,scalarMult(step_size,*df_x));
+      double f_x = state_ptr->f(*xold);
+      
+      int count=0;  
+      while(state_ptr->f(xnew)>f_x+armijo_coeff*step_size*dot(*df_x,*df_x) && count<25)
+      {
+	count++;
+	step_size *= step_factor;
+	xnew.clear();
+	xnew = add(*xold,scalarMult(step_size,*df_x));
+      }
+
+      state_ptr->x=xnew;//modifies value pointed to by xold
+    }
+    
+    //Constructor
+    gradDescent(algState& _state, double _step_size, double _step_factor, double _armijo_coeff)
+    {
+      step_size=_step_size;
+      step_factor=_step_factor;
+      armijo_coeff=_armijo_coeff;
+      state_ptr=&_state;
+    }
+    
+    //Constructor(overloaded)
+    gradDescent(algState& _state)
+    {
+      step_size=1.0;
+      step_factor=0.5;
+      armijo_coeff=1e-4;
+      state_ptr=&_state;
+    }
+    
+};//end of step class
 
 double cost_function(std::vector<double> x)
 {
@@ -114,6 +154,7 @@ double cost_function(std::vector<double> x)
 
 int main(int argc, char **argv) {
 
+    //for debugging and testing
     std::vector<double> x0;
     x0.resize(3);
     x0.at(0)=3.0;
@@ -124,9 +165,17 @@ int main(int argc, char **argv) {
     state.df.at(1)=-2.0;
     state.df.at(2)=-1.0;
     
-    lineSearch ls;
-    ls.state_ptr=&state;
-    printf("scalar func %f\n",ls.scalar_func(0.5));
+    gradDescent gd(state);
+//     gd.state_ptr=&state;
+//     gd.step_size=10.0;
+//     gd.armijo_coeff=1e-3;
+//     gd.step_factor=0.5;
+    printf("scalar func %f\n",gd.scalar_func(0.5));
+    
+    printf("state: (%f,%f,%f)\n",gd.state_ptr->x.at(0),gd.state_ptr->x.at(1),gd.state_ptr->x.at(2));
+    gd.armijo_step();
+    printf("new state: (%f,%f,%f)\n",gd.state_ptr->x.at(0),gd.state_ptr->x.at(1),gd.state_ptr->x.at(2));
+    printf("new cost: %f\n",gd.state_ptr->f(gd.state_ptr->x));
     //next need gradient from sampling a simplex
     //use armijo, forget exact minimization
     
